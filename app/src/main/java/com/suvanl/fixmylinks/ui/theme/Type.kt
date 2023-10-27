dd87f289374b62d7d7f83a8bf33b99700eb00e9a
suvanl <32813609+suvanl@users.noreply.github.com>
2023-10-27 19:06:15 +0100

    ✏️ (typography): rename `overusedGrotesk()` to `defaultCustomFont()`

--- app/src/main/java/com/suvanl/fixmylinks/ui/theme/Type.kt
+++ app/src/main/java/com/suvanl/fixmylinks/ui/theme/Type.kt
@@ -11,7 +11,7 @@ import androidx.compose.ui.unit.sp
 import com.suvanl.fixmylinks.R
 
 @OptIn(ExperimentalTextApi::class)
-private fun overusedGrotesk(
+private fun defaultCustomFont(
     variationSettings: FontVariation.Setting = FontVariation.weight(FontWeight.Normal.weight)
 ): FontFamily {
     return FontFamily(
@@ -26,70 +26,70 @@ private val defaultTypography = Typography()
 val Typography = Typography(
     // Display
     displayLarge = defaultTypography.displayLarge.copy(
-        fontFamily = overusedGrotesk()
+        fontFamily = defaultCustomFont()
     ),
     displayMedium = defaultTypography.displayMedium.copy(
-        fontFamily = overusedGrotesk()
+        fontFamily = defaultCustomFont()
     ),
     displaySmall = defaultTypography.displaySmall.copy(
-        fontFamily = overusedGrotesk()
+        fontFamily = defaultCustomFont()
     ),
 
     // Headline
     headlineLarge = defaultTypography.headlineLarge.copy(
-        fontFamily = overusedGrotesk()
+        fontFamily = defaultCustomFont()
     ),
     headlineMedium = defaultTypography.headlineMedium.copy(
-        fontFamily = overusedGrotesk()
+        fontFamily = defaultCustomFont()
     ),
     headlineSmall = defaultTypography.headlineSmall.copy(
-        fontFamily = overusedGrotesk()
+        fontFamily = defaultCustomFont()
     ),
 
     // Title
     titleLarge = defaultTypography.titleLarge.copy(
-        fontFamily = overusedGrotesk()
+        fontFamily = defaultCustomFont()
     ),
     titleMedium = defaultTypography.titleMedium.copy(
-        fontFamily = overusedGrotesk(
+        fontFamily = defaultCustomFont(
             FontVariation.weight(FontWeight.Medium.weight)
         )
     ),
     titleSmall = defaultTypography.titleSmall.copy(
-        fontFamily = overusedGrotesk(
+        fontFamily = defaultCustomFont(
             FontVariation.weight(FontWeight.Medium.weight)
         )
     ),
 
     // Label
     labelLarge = defaultTypography.labelLarge.copy(
-        fontFamily = overusedGrotesk(
+        fontFamily = defaultCustomFont(
             FontVariation.weight(FontWeight.Medium.weight)
         )
     ),
     labelMedium = defaultTypography.labelMedium.copy(
-        fontFamily = overusedGrotesk(
+        fontFamily = defaultCustomFont(
             FontVariation.weight(FontWeight.Medium.weight)
         )
     ),
     labelSmall = defaultTypography.labelSmall.copy(
-        fontFamily = overusedGrotesk(
+        fontFamily = defaultCustomFont(
             FontVariation.weight(FontWeight.Medium.weight)
         )
     ),
 
     // Body
     bodyLarge = TextStyle(
-        fontFamily = overusedGrotesk(),
+        fontFamily = defaultCustomFont(),
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     ),
     bodyMedium = defaultTypography.bodyMedium.copy(
-        fontFamily = overusedGrotesk()
+        fontFamily = defaultCustomFont()
     ),
     bodySmall = defaultTypography.bodySmall.copy(
-        fontFamily = overusedGrotesk()
+        fontFamily = defaultCustomFont()
     )
 )
