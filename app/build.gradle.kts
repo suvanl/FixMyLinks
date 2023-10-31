b14b83630525d5e315136d830051e4b98760df2e
suvanl <32813609+suvanl@users.noreply.github.com>
2023-10-31 20:19:31 +0000

    ⬇️ Change Kotlin version to 1.9.10 ⬆️ Upgrade Compose compiler version to 1.5.3 (latest)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.guardsquare.appsweep")
-    id("com.google.devtools.ksp") version "1.9.0-1.0.13"
+    id("com.google.devtools.ksp") version "1.9.10-1.0.13"
 }
 
 android {
@@ -42,7 +42,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.5.3"
     }
     packaging {
         resources {
