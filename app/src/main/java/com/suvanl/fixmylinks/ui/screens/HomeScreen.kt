4e52f029f2f79fed3458528060ad0f08da312821
suvanl <32813609+suvanl@users.noreply.github.com>
2023-11-03 17:10:25 +0000

    💄 (HomeScreen): align content centrally

--- app/src/main/java/com/suvanl/fixmylinks/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/suvanl/fixmylinks/ui/screens/HomeScreen.kt
@@ -1,14 +1,14 @@
 package com.suvanl.fixmylinks.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
@@ -21,12 +21,14 @@ import com.suvanl.fixmylinks.ui.theme.FixMyLinksTheme
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
     Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
         modifier = modifier
             .fillMaxSize()
             .verticalScroll(rememberScrollState())
             .semantics { contentDescription = "Home Screen" }
     ) {
-        Spacer(modifier = Modifier.height(16.dp))
+        //Spacer(modifier = Modifier.height(16.dp))
 
         Greeting(
             name = stringResource(id = R.string.app_name),
