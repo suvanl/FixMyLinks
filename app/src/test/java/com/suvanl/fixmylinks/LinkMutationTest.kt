ad7faf2b2be93e5c6298383986d89ec6aac1785b
suvanl <32813609+suvanl@users.noreply.github.com>
2023-11-06 12:07:34 +0000

    ✅ (LinkMutationTest): init `MutateUriUseCase` in `setup()` instead of explicitly doing so within each test case

--- app/src/test/java/com/suvanl/fixmylinks/LinkMutationTest.kt
+++ app/src/test/java/com/suvanl/fixmylinks/LinkMutationTest.kt
@@ -2,6 +2,7 @@ package com.suvanl.fixmylinks
 
 import com.suvanl.fixmylinks.domain.mutation.MutateUriUseCase
 import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
 import java.net.URI
 
@@ -9,9 +10,16 @@ import java.net.URI
  * Test suite for link mutation business logic
  */
 class LinkMutationTest {
+
+    private lateinit var mutateUriUseCase: MutateUriUseCase
+
+    @Before
+    fun setup() {
+        mutateUriUseCase = MutateUriUseCase()
+    }
+
     @Test
     fun `remove ONLY 'igshid' parameter from Instagram link with multiple parameters`() {
-        val mutateUriUseCase = MutateUriUseCase()
         val actual = mutateUriUseCase(
             URI("https://www.instagram.com/reel/CxdeuhUrrE6/?igshid=MTc4MmM1YmI2Ng==&hl=en")
         )
@@ -22,7 +30,6 @@ class LinkMutationTest {
 
     @Test
     fun `remove ONLY 'igshid' parameter from Instagram link with multiple parameters (reversed param order)`() {
-        val mutateUriUseCase = MutateUriUseCase()
         val actual = mutateUriUseCase(
             URI("https://www.instagram.com/reel/CxdeuhUrrE6/?hl=en&igshid=MTc4MmM1YmI2Ng==")
         )
@@ -33,7 +40,6 @@ class LinkMutationTest {
 
     @Test
     fun `remove lone 'igshid' parameter from Instagram link using URL_PARAMS_SPECIFIC MutationType`() {
-        val mutateUriUseCase = MutateUriUseCase()
         val actual = mutateUriUseCase(
             URI("https://www.instagram.com/reel/CxdeuhUrrE6/?igshid=MTc4MmM1YmI2Ng==")
         )
@@ -44,7 +50,6 @@ class LinkMutationTest {
 
     @Test
     fun `remove lone 'igshid' parameter from Instagram link with urlencoded chars using URL_PARAMS_SPECIFIC MutationType`() {
-        val mutateUriUseCase = MutateUriUseCase()
         val actual = mutateUriUseCase(
             URI("https://www.instagram.com/reel/CxdeuhUrrE6/?igshid=MTc4MmM1YmI2Ng%3D%3D")
         )
@@ -55,7 +60,6 @@ class LinkMutationTest {
 
     @Test
     fun `change x(dot)com domain name to twitter(dot)com and remove all URL parameters`() {
-        val mutateUriUseCase = MutateUriUseCase()
         val actual = mutateUriUseCase(
             URI("https://x.com/Android/status/1704894351976137098?t=TZO0gtzWyOO95dFV8JzGXw&s=09")
         )
@@ -66,7 +70,6 @@ class LinkMutationTest {
 
     @Test
     fun `change www(dot)x(dot)com domain name to www(dot)twitter(dot)com and remove all URL parameters`() {
-        val mutateUriUseCase = MutateUriUseCase()
         val actual = mutateUriUseCase(
             URI("https://www.x.com/Android/status/1704894351976137098?t=TZO0gtzWyOO95dFV8JzGXw&s=09")
         )
@@ -77,7 +80,6 @@ class LinkMutationTest {
 
     @Test
     fun `attempt to remove URL parameters of a parameterless Spotify link`() {
-        val mutateUriUseCase = MutateUriUseCase()
         val actual = mutateUriUseCase(
             URI("https://open.spotify.com/track/3PRljkcobGtC6Kc3ILLSmq")
         )
