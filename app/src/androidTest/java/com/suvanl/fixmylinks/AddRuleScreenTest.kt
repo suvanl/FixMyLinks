55b703d3a9321c86112e9e0b3bc904b6bb2a97db
suvanl <32813609+suvanl@users.noreply.github.com>
2023-11-22 22:35:36 +0000

    ✅ (AddRuleScreenTest): use correct parameters in *Form composable invocations

--- app/src/androidTest/java/com/suvanl/fixmylinks/AddRuleScreenTest.kt
+++ app/src/androidTest/java/com/suvanl/fixmylinks/AddRuleScreenTest.kt
@@ -13,9 +13,11 @@ import androidx.compose.ui.test.onNodeWithTag
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.suvanl.fixmylinks.domain.mutation.MutationType
 import com.suvanl.fixmylinks.ui.components.form.AllUrlParamsRuleForm
+import com.suvanl.fixmylinks.ui.components.form.AllUrlParamsRuleFormState
 import com.suvanl.fixmylinks.ui.components.form.DomainNameRuleForm
 import com.suvanl.fixmylinks.ui.components.form.DomainNameRuleFormState
 import com.suvanl.fixmylinks.ui.components.form.SpecificUrlParamsRuleForm
+import com.suvanl.fixmylinks.ui.components.form.SpecificUrlParamsRuleFormState
 import com.suvanl.fixmylinks.ui.components.list.SwitchListItemState
 import com.suvanl.fixmylinks.ui.screens.newruleflow.AddRuleScreenBody
 import org.junit.Rule
@@ -60,9 +62,7 @@ class AddRuleScreenTest {
                 MutationType.URL_PARAMS_SPECIFIC -> {
                     SpecificUrlParamsRuleForm(
                         showHints = true,
-                        addedParamNames = listOf(),
-                        ruleNameText = "",
-                        domainNameText = "",
+                        formState = SpecificUrlParamsRuleFormState(),
                         onRuleNameChange = {},
                         onDomainNameChange = {},
                         onClickAddParam = {},
@@ -83,8 +83,7 @@ class AddRuleScreenTest {
                 MutationType.URL_PARAMS_ALL -> {
                     AllUrlParamsRuleForm(
                         showHints = true,
-                        ruleNameText = "",
-                        domainNameText = "",
+                        formState = AllUrlParamsRuleFormState(),
                         onRuleNameChange = {},
                         onDomainNameChange = {}
                     )
