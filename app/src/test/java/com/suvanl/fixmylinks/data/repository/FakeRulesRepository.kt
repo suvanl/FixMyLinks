0e6663e4e762cd674d48fb69d8fe81ddc1d1063a
suvanl <32813609+suvanl@users.noreply.github.com>
2023-12-04 14:59:12 +0000

    ✅ Add ViewModel tests for updating existing rules

--- app/src/test/java/com/suvanl/fixmylinks/data/repository/FakeRulesRepository.kt
+++ app/src/test/java/com/suvanl/fixmylinks/data/repository/FakeRulesRepository.kt
@@ -1,9 +1,18 @@
 package com.suvanl.fixmylinks.data.repository
 
+import com.suvanl.fixmylinks.domain.mutation.MutationType
+import com.suvanl.fixmylinks.domain.mutation.model.AllUrlParamsMutationModel
 import com.suvanl.fixmylinks.domain.mutation.model.BaseMutationModel
+import com.suvanl.fixmylinks.domain.mutation.model.DomainNameAndAllUrlParamsMutationModel
+import com.suvanl.fixmylinks.domain.mutation.model.DomainNameMutationInfo
+import com.suvanl.fixmylinks.domain.mutation.model.DomainNameMutationModel
+import com.suvanl.fixmylinks.domain.mutation.model.SpecificUrlParamsMutationInfo
+import com.suvanl.fixmylinks.domain.mutation.model.SpecificUrlParamsMutationModel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.runBlocking
 
 class FakeRulesRepository : RulesRepository {
@@ -13,6 +22,18 @@ class FakeRulesRepository : RulesRepository {
 
     override suspend fun saveRule(rule: BaseMutationModel) {
         customRules += rule
+        refreshFlow()
+    }
+
+    override suspend fun updateRule(baseRuleId: Long, newData: BaseMutationModel) {
+        val ruleIndex = customRulesStream.first().indexOfFirst { it.baseRuleId == baseRuleId }
+        if (ruleIndex == -1) {
+            throw IllegalArgumentException("Couldn't find a rule with baseRuleId $baseRuleId")
+        }
+
+        customRules.removeAt(ruleIndex)
+        customRules.add(ruleIndex, newData)
+
         refreshFlow()
     }
 
@@ -29,6 +50,18 @@ class FakeRulesRepository : RulesRepository {
         refreshFlow()
     }
 
+    override fun getRuleByBaseId(
+        baseRuleId: Long,
+        ruleType: MutationType
+    ): Flow<BaseMutationModel> {
+        runBlocking { refreshFlow() }
+
+        return customRulesStream.map { rules ->
+            rules.find { it.baseRuleId == baseRuleId && it.mutationType == ruleType }
+                ?: throw NullPointerException("Couldn't find a rule with baseRuleId $baseRuleId or mutationType $ruleType")
+        }
+    }
+
     override fun getAllRules(): Flow<List<BaseMutationModel>> {
         runBlocking {
             refreshFlow()
@@ -37,7 +70,67 @@ class FakeRulesRepository : RulesRepository {
         return customRulesStream
     }
 
+    suspend fun insertFakeData() {
+        fakeRules.forEach {
+            saveRule(it)
+        }
+
+        refreshFlow()
+    }
+
     private suspend fun refreshFlow() {
         customRulesStream = flow { emit(customRules) }
+    }
+
+    companion object {
+        private val fakeRules = listOf(
+            AllUrlParamsMutationModel(
+                baseRuleId = 1,
+                name = "My rule",
+                triggerDomain = "instagram.com",
+                isLocalOnly = true,
+            ),
+            AllUrlParamsMutationModel(
+                baseRuleId = 2,
+                name = "My second rule",
+                triggerDomain = "spotify.com",
+                isLocalOnly = true,
+            ),
+            DomainNameMutationModel(
+                baseRuleId = 3,
+                name = "twitter to x",
+                triggerDomain = "x.com",
+                isLocalOnly = true,
+                mutationInfo = DomainNameMutationInfo(
+                    initialDomain = "twitter.com",
+                    targetDomain = "x.com",
+                ),
+            ),
+            AllUrlParamsMutationModel(
+                baseRuleId = 4,
+                name = "My second rule",
+                triggerDomain = "spotify.com",
+                isLocalOnly = true,
+            ),
+            DomainNameAndAllUrlParamsMutationModel(
+                baseRuleId = 5,
+                name = "Android Dev",
+                triggerDomain = "developer.android.com",
+                isLocalOnly = true,
+                mutationInfo = DomainNameMutationInfo(
+                    initialDomain = "developer.android.com",
+                    targetDomain = "d.android.com",
+                ),
+            ),
+            SpecificUrlParamsMutationModel(
+                baseRuleId = 6,
+                name = "YouTube - remove playlist association",
+                triggerDomain = "youtube.com",
+                isLocalOnly = true,
+                mutationInfo = SpecificUrlParamsMutationInfo(
+                    removableParams = listOf("list"),
+                ),
+            ),
+        )
     }
 }
--- app/src/test/java/com/suvanl/fixmylinks/viewmodel/newruleflow/AddAllUrlParamsRuleViewModelTest.kt
+++ app/src/test/java/com/suvanl/fixmylinks/viewmodel/newruleflow/AddAllUrlParamsRuleViewModelTest.kt
@@ -5,12 +5,18 @@ import com.suvanl.fixmylinks.data.repository.FakeRulesRepository
 import com.suvanl.fixmylinks.data.repository.PreferencesRepository
 import com.suvanl.fixmylinks.data.repository.RulesRepository
 import com.suvanl.fixmylinks.data.repository.UserPreferences
+import com.suvanl.fixmylinks.domain.mutation.MutationType
+import com.suvanl.fixmylinks.domain.mutation.model.AllUrlParamsMutationModel
 import com.suvanl.fixmylinks.domain.validation.ValidateDomainNameUseCase
+import com.suvanl.fixmylinks.ui.components.form.AllUrlParamsRuleFormState
 import com.suvanl.fixmylinks.viewmodel.newruleflow.util.FakeDomainNameValidator
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertNull
+import org.junit.Assert.assertThrows
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 
@@ -67,6 +73,52 @@ class AddAllUrlParamsRuleViewModelTest {
             // Assert that the rule hasn't been saved in the data source
             val rule = allRules.find { it.name == ruleName }
             assertNull(rule)
+        }
+    }
+
+    @Test
+    fun `updating an existing rule results in it being replaced with new data`() {
+        val baseRuleId = 2L
+        val formUiState = AllUrlParamsRuleFormState(
+            ruleName = "My Spotify rule",
+            domainName = "www.spotify.com",
+        )
+
+        val newData = AllUrlParamsMutationModel(
+            baseRuleId = baseRuleId,
+            name = formUiState.ruleName,
+            triggerDomain = formUiState.domainName,
+            isLocalOnly = true,
+        )
+
+        runBlocking {
+            (rulesRepository as FakeRulesRepository).insertFakeData()
+            rulesRepository.updateRule(baseRuleId, newData)
+
+            // verify update
+            val updated = rulesRepository.getRuleByBaseId(baseRuleId, MutationType.URL_PARAMS_ALL).first()
+            assertTrue(updated is AllUrlParamsMutationModel)
+            assertEquals(newData, updated)
+        }
+    }
+
+    @Test
+    fun `updating a rule that doesn't exist throws an exception`() {
+        val baseRuleId = 20L
+        val formUiState = AllUrlParamsRuleFormState()
+        val newData = AllUrlParamsMutationModel(
+            baseRuleId = baseRuleId,
+            name = formUiState.ruleName,
+            triggerDomain = formUiState.domainName,
+            isLocalOnly = true,
+        )
+
+        // should throw an IAE if a rule with the given baseRuleId can't be found in the data source
+        assertThrows(IllegalArgumentException::class.java) {
+            runBlocking {
+                (rulesRepository as FakeRulesRepository).insertFakeData()
+                rulesRepository.updateRule(baseRuleId, newData)
+            }
         }
     }
 
