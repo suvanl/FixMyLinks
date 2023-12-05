6813ab24d9e87d24f20c1fd09490c010b9ab1857
suvanl <32813609+suvanl@users.noreply.github.com>
2023-12-05 22:06:09 +0000

    💄 Show "No rules added yet" body on RulesScreen if no rules have been saved yet

--- app/src/main/java/com/suvanl/fixmylinks/ui/screens/RulesScreen.kt
+++ app/src/main/java/com/suvanl/fixmylinks/ui/screens/RulesScreen.kt
@@ -1,28 +1,39 @@
 package com.suvanl.fixmylinks.ui.screens
 
 import android.content.res.Configuration
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Link
 import androidx.compose.material3.Button
 import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.suvanl.fixmylinks.R
 import com.suvanl.fixmylinks.domain.mutation.MutationType
 import com.suvanl.fixmylinks.domain.mutation.model.BaseMutationModel
 import com.suvanl.fixmylinks.domain.mutation.model.DomainNameAndAllUrlParamsMutationModel
@@ -71,55 +82,114 @@ private fun RulesScreenBody(
     onClickDeleteRule: (Long) -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val hasRules = uiState.rules.isNotEmpty()
+
     Column(
+        horizontalAlignment = if (hasRules) Alignment.Start else Alignment.CenterHorizontally,
+        verticalArrangement = if (hasRules) Arrangement.Top else Arrangement.Center,
         modifier = modifier
             .fillMaxSize()
             .semantics { contentDescription = "Rules Screen" }
     ) {
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(
-            onClick = onClickDeleteAll
-        ) {
-            Text(text = "Delete all")
+        if (!hasRules) {
+            EmptyRulesBody()
+            return
         }
 
-        LazyColumn(
-            verticalArrangement = Arrangement.spacedBy(16.dp)
-        ) {
-            items(items = uiState.rules.sortedByDescending { it.dateModifiedTimestamp }) { rule ->
-                Column {
-                    Text(
-                        text = rule.name,
-                        style = MaterialTheme.typography.titleLarge
-                    )
+        RulesList(
+            uiState = uiState,
+            onClickDeleteAll = onClickDeleteAll,
+            onClickDeleteRule = onClickDeleteRule,
+            onClickItem = onClickItem
+        )
+    }
+}
 
-                    Text(
-                        text = "${rule.mutationType.name} on ${rule.triggerDomain}"
-                    )
+@Composable
+private fun RulesList(
+    uiState: RulesScreenUiState,
+    onClickDeleteAll: () -> Unit,
+    onClickDeleteRule: (Long) -> Unit,
+    onClickItem: (MutationType, Long) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Spacer(modifier = Modifier.height(16.dp))
 
-                    Text(
-                        text = "Last modified ${rule.dateModifiedTimestamp}",
-                        style = MaterialTheme.typography.labelMedium
-                    )
+    Button(
+        onClick = onClickDeleteAll
+    ) {
+        Text(text = "Delete all")
+    }
 
-                    Row(horizontalArrangement = Arrangement.SpaceBetween) {
-                        FilledTonalButton(onClick = { onClickDeleteRule(rule.baseRuleId) }) {
-                            Text(text = "Delete this")
-                        }
-                        FilledTonalButton(
-                            onClick = {
-                                onClickItem(rule.mutationType, rule.baseRuleId)
-                            }
-                        ) {
-                            Text(text = "View this")
-                        }
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = modifier
+    ) {
+        items(items = uiState.rules.sortedByDescending { it.dateModifiedTimestamp }) { rule ->
+            Column {
+                Text(
+                    text = rule.name,
+                    style = MaterialTheme.typography.titleLarge
+                )
+
+                Text(
+                    text = "${rule.mutationType.name} on ${rule.triggerDomain}"
+                )
+
+                Text(
+                    text = "Last modified ${rule.dateModifiedTimestamp}",
+                    style = MaterialTheme.typography.labelMedium
+                )
+
+                Row(horizontalArrangement = Arrangement.SpaceBetween) {
+                    FilledTonalButton(onClick = { onClickDeleteRule(rule.baseRuleId) }) {
+                        Text(text = "Delete this")
                     }
-
-                    Spacer(modifier = Modifier.height(16.dp))
+                    FilledTonalButton(
+                        onClick = {
+                            onClickItem(rule.mutationType, rule.baseRuleId)
+                        }
+                    ) {
+                        Text(text = "View this")
+                    }
                 }
+
+                Spacer(modifier = Modifier.height(16.dp))
             }
         }
+    }
+}
+
+@Composable
+private fun EmptyRulesBody(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = modifier
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .background(
+                    color = MaterialTheme.colorScheme.secondaryContainer,
+                    shape = CircleShape
+                )
+                .padding(48.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Outlined.Link,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.onSecondaryContainer,
+                modifier = Modifier.size(100.dp)
+            )
+        }
+
+        Text(
+            text = stringResource(R.string.no_rules_added_yet),
+            style = MaterialTheme.typography.titleLarge
+        )
     }
 }
 
