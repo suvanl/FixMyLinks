suvanl/FixMyLinks

commit 947ec660352d5ea8f6786fa584310f1f088b5327

♻️ Refactor how rule data is shared between RulesScreen and RuleDetailsScreen
The new approach involves using a shared ViewModel scoped to a nested navigation graph, containing the FmlScreen.Rules and FmlScreen.RuleDetails destinations.

This ViewModel contains the state of the currently selected rule, which RuleDetailsScreen collects and displays visually.

app/src/main/java/com/suvanl/fixmylinks/ui/navigation/FmlNavHost.kt
@@ -3,6 +3,7 @@ package com.suvanl.fixmylinks.ui.navigation
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Text
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
@@ -14,6 +15,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -40,7 +42,9 @@ import com.suvanl.fixmylinks.ui.screens.details.RuleDetailsScreen
 import com.suvanl.fixmylinks.ui.screens.newruleflow.AddRuleScreen
 import com.suvanl.fixmylinks.ui.screens.newruleflow.AddRuleScreenUiState
 import com.suvanl.fixmylinks.ui.screens.newruleflow.SelectRuleTypeScreen
+import com.suvanl.fixmylinks.ui.theme.TextStyleDefaults
 import com.suvanl.fixmylinks.ui.util.getNewRuleFlowViewModel
+import com.suvanl.fixmylinks.viewmodel.RulesViewModel
 import com.suvanl.fixmylinks.viewmodel.newruleflow.AddRuleViewModel
 import com.suvanl.fixmylinks.viewmodel.newruleflow.SelectRuleTypeViewModel
 import kotlinx.coroutines.launch
@@ -72,40 +76,74 @@ fun FmlNavHost(
             HomeScreen()
         }
 
-        composable(route = FmlScreen.Rules.route) {
-            RulesScreen(
-                onClickRuleItem = { mutationType, baseRuleId ->
-                    navController.navigateSingleTop(
-                        route = "${FmlScreen.RuleDetails.route}/${mutationType.name}/${baseRuleId}",
-                        popUpToStartDestination = false
-                    )
-                }
-            )
-        }
-
         composable(route = FmlScreen.Saved.route) {
             SavedScreen()
         }
 
-        composable(
-            route = FmlScreen.RuleDetails.routeWithArgs,
-            arguments = FmlScreen.RuleDetails.args
-        ) { navBackStackEntry ->
-            val mutationTypeArg =
-                navBackStackEntry.arguments?.getString(FmlScreen.RuleDetails.mutationTypeArg)
+        navigation(
+            startDestination = FmlScreen.Rules.route,
+            route = NestedNavGraphParent.RuleDetailsFlow.route
+        ) {
+            composable(route = FmlScreen.Rules.route) { navBackStackEntry ->
+                val viewModel = navBackStackEntry.sharedViewModel<RulesViewModel>(navController)
+                val uiState by viewModel.rulesScreenUiState.collectAsStateWithLifecycle()
 
-            val mutationType = MutationType.entries.find { it.name == mutationTypeArg }
-                ?: MutationType.FALLBACK
+                RulesScreen(
+                    uiState = uiState,
+                    onClickRuleItem = { rule ->
+                        viewModel.setSelectedRule(rule)
+                        navController.navigateSingleTop(
+                            route = FmlScreen.RuleDetails.route,
+                            popUpToStartDestination = false
+                        )
+                    }
+                )
+            }
 
-            val baseRuleId =
-                navBackStackEntry.arguments?.getLong(FmlScreen.RuleDetails.baseRuleIdArg)
-                    ?: throw NullPointerException("Expected base_rule_id to be non-null")
+            composable(
+                route = FmlScreen.RuleDetails.route
+            ) { navBackStackEntry ->
+                val viewModel = navBackStackEntry.sharedViewModel<RulesViewModel>(navController)
+                val selectedRule by viewModel.selectedRule.collectAsStateWithLifecycle()
+                val deleteConfirmationRequired by viewModel.deleteConfirmationRequired.collectAsStateWithLifecycle()
 
-            RuleDetailsScreen(
-                mutationType = mutationType,
-                baseRuleId = baseRuleId,
-                onClickEdit = {}
-            )
+                val coroutineScope = rememberCoroutineScope()
+
+                ProvideAppBarActions {
+                    OverflowMenu {
+                        DropdownMenuItem(
+                            text = {
+                                Text(
+                                    text = stringResource(id = R.string.delete),
+                                    style = TextStyleDefaults.dropdownItemStyle,
+                                )
+                            },
+                            onClick = {
+                                if (selectedRule != null) {
+                                    viewModel.setDeleteConfirmationRequired(true)
+                                }
+                            }
+                        )
+                    }
+                }
+
+                RuleDetailsScreen(
+                    rule = selectedRule,
+                    showDeleteConfirmation = deleteConfirmationRequired,
+                    onDismissDeleteConfirmation = {
+                        viewModel.setDeleteConfirmationRequired(false)
+                    },
+                    onDelete = {
+                        coroutineScope.launch {
+                            viewModel.deleteSingleRule(selectedRule!!.baseRuleId)
+                            navController.popBackStack(
+                                route = FmlScreen.Rules.route,
+                                inclusive = false
+                            )
+                        }
+                    }
+                )
+            }
         }
 
         navigation(
@@ -275,7 +313,7 @@ fun NavHostController.navigateSingleTop(route: String, popUpToStartDestination: 
 private inline fun <reified VM : ViewModel> NavBackStackEntry.sharedViewModel(
     navController: NavController
 ): VM {
-    val navGraphRoute = destination.parent?.route ?: return viewModel()
+    val navGraphRoute = destination.parent?.route ?: return hiltViewModel()
     val parentEntry = remember(this) {
         navController.getBackStackEntry(navGraphRoute)
     }
@@ -283,5 +321,5 @@ private inline fun <reified VM : ViewModel> NavBackStackEntry.sharedViewModel(
     // Scope VM to nested nav graph parent entry to ensure that the VM won't be cleared if a single
     // screen within the nested nav graph is popped, but rather only if the whole nested nav graph
     // gets popped off the back stack
-    return viewModel(parentEntry)
+    return hiltViewModel(parentEntry)
 }

app/src/main/java/com/suvanl/fixmylinks/viewmodel/RulesViewModel.kt
@@ -3,10 +3,13 @@ package com.suvanl.fixmylinks.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.suvanl.fixmylinks.data.repository.RulesRepository
+import com.suvanl.fixmylinks.domain.mutation.model.BaseMutationModel
 import com.suvanl.fixmylinks.ui.screens.RulesScreenUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
@@ -24,12 +27,25 @@ class RulesViewModel @Inject constructor(
                 initialValue = RulesScreenUiState()
             )
 
-    suspend fun deleteSingleRule(baseRuleId: Long) {
-        rulesRepository.deleteByBaseRuleId(baseRuleId)
+    private val _selectedRule = MutableStateFlow<BaseMutationModel?>(null)
+    val selectedRule = _selectedRule.asStateFlow()
+
+    // whether a selectedRule requires delete confirmation
+    private val _deleteConfirmationRequired = MutableStateFlow(false)
+    val deleteConfirmationRequired = _deleteConfirmationRequired.asStateFlow()
+
+    fun setSelectedRule(rule: BaseMutationModel?) {
+        _selectedRule.value = rule
     }
 
-    suspend fun deleteAll() {
-        rulesRepository.deleteAllRules()
+    fun setDeleteConfirmationRequired(isRequired: Boolean) {
+        _deleteConfirmationRequired.value = isRequired
+    }
+
+    suspend fun deleteSingleRule(baseRuleId: Long) {
+        setSelectedRule(null)
+        rulesRepository.deleteByBaseRuleId(baseRuleId)
+        setDeleteConfirmationRequired(false)
     }
 
     companion object {

app/src/main/java/com/suvanl/fixmylinks/ui/components/list/RulesList.kt
@@ -10,7 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.suvanl.fixmylinks.domain.mutation.MutationType
+import com.suvanl.fixmylinks.domain.mutation.model.BaseMutationModel
 import com.suvanl.fixmylinks.ui.screens.RulesScreenUiState
 import com.suvanl.fixmylinks.ui.util.PreviewContainer
 import com.suvanl.fixmylinks.ui.util.PreviewData
@@ -18,7 +18,7 @@ import com.suvanl.fixmylinks.ui.util.PreviewData
 @Composable
 fun RulesList(
     uiState: RulesScreenUiState,
-    onClickItem: (MutationType, Long) -> Unit,
+    onClickItem: (BaseMutationModel) -> Unit,
     modifier: Modifier = Modifier
 ) {
     val items = uiState.rules.sortedByDescending { it.dateModifiedTimestamp }
@@ -30,7 +30,7 @@ fun RulesList(
         items(items = items) { rule ->
             RulesListItem(
                 rule = rule,
-                onClick = { mutationType, baseRuleId -> onClickItem(mutationType, baseRuleId) }
+                onClick = { onClickItem(it) }
             )
         }
 
@@ -55,7 +55,7 @@ private fun RulesListPreview() {
     PreviewContainer {
         RulesList(
             uiState = RulesScreenUiState(rules = PreviewData.previewRules),
-            onClickItem = { _, _ -> /* do nothing */ }
+            onClickItem = { /* do nothing */ }
         )
     }
 }

app/src/main/java/com/suvanl/fixmylinks/ui/components/list/RulesListItem.kt
@@ -47,7 +47,7 @@ private fun getShapeForRule(ruleType: MutationType): Shape = when (ruleType) {
 @Composable
 fun RulesListItem(
     rule: BaseMutationModel,
-    onClick: (MutationType, Long) -> Unit,
+    onClick: (BaseMutationModel) -> Unit,
     modifier: Modifier = Modifier,
 ) {
     Card(
@@ -91,7 +91,7 @@ fun RulesListItem(
             modifier = Modifier
                 .padding(4.dp)
                 .clickable {
-                    onClick(rule.mutationType, rule.baseRuleId)
+                    onClick(rule)
                 }
         )
     }
@@ -103,7 +103,7 @@ private fun ItemPreview() {
     PreviewContainer {
         RulesListItem(
             rule = PreviewData.previewRules[1],
-            onClick = { _, _ -> /* do nothing */ },
+            onClick = { /* do nothing */ },
             modifier = Modifier.padding(8.dp)
         )
     }

app/src/main/java/com/suvanl/fixmylinks/ui/navigation/NestedNavGraphParent.kt
@@ -2,4 +2,5 @@ package com.suvanl.fixmylinks.ui.navigation
 
 sealed class NestedNavGraphParent(val route: String) {
     data object NewRuleFlow : NestedNavGraphParent("new_rule_flow")
+    data object RuleDetailsFlow : NestedNavGraphParent("rule_details_flow")
 }

app/src/main/java/com/suvanl/fixmylinks/ui/screens/RulesScreen.kt
@@ -14,7 +14,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -22,17 +21,13 @@ import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.suvanl.fixmylinks.R
-import com.suvanl.fixmylinks.domain.mutation.MutationType
 import com.suvanl.fixmylinks.domain.mutation.model.BaseMutationModel
 import com.suvanl.fixmylinks.ui.components.list.RulesList
-import com.suvanl.fixmylinks.ui.layout.Polygon
 import com.suvanl.fixmylinks.ui.graphics.CustomShapes.ScallopPolygon
+import com.suvanl.fixmylinks.ui.layout.Polygon
 import com.suvanl.fixmylinks.ui.util.PreviewContainer
 import com.suvanl.fixmylinks.ui.util.PreviewData
-import com.suvanl.fixmylinks.viewmodel.RulesViewModel
 
 data class RulesScreenUiState(
     val rules: List<BaseMutationModel> = listOf()
@@ -40,16 +35,14 @@ data class RulesScreenUiState(
 
 @Composable
 fun RulesScreen(
-    onClickRuleItem: (MutationType, Long) -> Unit,
+    uiState: RulesScreenUiState,
+    onClickRuleItem: (BaseMutationModel) -> Unit,
     modifier: Modifier = Modifier,
-    viewModel: RulesViewModel = hiltViewModel()
 ) {
-    val uiState by viewModel.rulesScreenUiState.collectAsStateWithLifecycle()
-
     RulesScreenBody(
         uiState = uiState,
-        onClickItem = { mutationType, baseRuleId ->
-            onClickRuleItem(mutationType, baseRuleId)
+        onClickItem = { rule ->
+            onClickRuleItem(rule)
         },
         modifier = modifier
     )
@@ -58,7 +51,7 @@ fun RulesScreen(
 @Composable
 private fun RulesScreenBody(
     uiState: RulesScreenUiState,
-    onClickItem: (MutationType, Long) -> Unit,
+    onClickItem: (BaseMutationModel) -> Unit,
     modifier: Modifier = Modifier
 ) {
     val hasRules = uiState.rules.isNotEmpty()
@@ -132,7 +125,7 @@ private fun RulesScreenPreview() {
     PreviewContainer {
         RulesScreenBody(
             uiState = RulesScreenUiState(rules = PreviewData.previewRules),
-            onClickItem = { _, _ -> /* do nothing */ },
+            onClickItem = { /* do nothing */ },
         )
     }
 }

app/src/main/java/com/suvanl/fixmylinks/ui/screens/details/RuleDetailsScreen.kt
@@ -2,41 +2,95 @@ package com.suvanl.fixmylinks.ui.screens.details
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.suvanl.fixmylinks.domain.mutation.MutationType
-import com.suvanl.fixmylinks.viewmodel.rule.RuleDetailsViewModel
+import com.suvanl.fixmylinks.R
+import com.suvanl.fixmylinks.domain.mutation.model.BaseMutationModel
+import com.suvanl.fixmylinks.ui.theme.LetterSpacingDefaults
 
+/**
+ * Displays additional info about the rule compared to that of
+ * [com.suvanl.fixmylinks.ui.screens.RulesScreen].
+ *
+ * @param rule The rule data to be displayed on this screen.
+ * @param showDeleteConfirmation Whether to display the "Delete rule" [AlertDialog].
+ * @param onDismissDeleteConfirmation When the "Delete rule" [AlertDialog] is dismissed.
+ * @param onDelete When the user clicks the `confirmButton` on the "Delete rule" dialog.
+ * @param modifier The default modifier for this screen.
+ */
 @Composable
 fun RuleDetailsScreen(
-    mutationType: MutationType,
-    baseRuleId: Long,
-    onClickEdit: () -> Unit,
+    rule: BaseMutationModel?,
+    showDeleteConfirmation: Boolean,
+    onDismissDeleteConfirmation: () -> Unit,
+    onDelete: () -> Unit,
     modifier: Modifier = Modifier,
-    viewModel: RuleDetailsViewModel = hiltViewModel(),
 ) {
-    LaunchedEffect(Unit) {
-        viewModel.updateSelectedRule(baseRuleId, mutationType)
+    if (rule == null) {
+        return
     }
-
-    val rule by viewModel.selectedRule.collectAsStateWithLifecycle()
 
     Column(
         modifier = modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp)
     ) {
+        if (showDeleteConfirmation) {
+            DeleteConfirmationDialog(
+                onDismissRequest = { onDismissDeleteConfirmation() },
+                onConfirmDelete = { onDelete() }
+            )
+        }
+
         Text(text = "Rule details")
-        Text(text = "type: $mutationType")
-        Text(text = "base rule ID: $baseRuleId")
+        Text(text = "type: ${rule.mutationType}")
+        Text(text = "base rule ID: ${rule.baseRuleId}")
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Text(text = "Name: ${rule?.name}; On: ${rule?.triggerDomain}")
+        Text(text = "Name: ${rule.name}; On: ${rule.triggerDomain}")
     }
+}
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDismissRequest: () -> Unit,
+    onConfirmDelete: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    AlertDialog(
+        title = {
+            Text(
+                text = "Delete rule",
+                letterSpacing = LetterSpacingDefaults.Tighter,
+            )
+        },
+        text = {
+            Text(text = "Are you sure you want to delete this rule? This action can't be undone.")
+        },
+        dismissButton = {
+            TextButton(
+                onClick = { onDismissRequest() }
+            ) {
+                Text(text = stringResource(id = android.R.string.cancel))
+            }
+        },
+        confirmButton = {
+            TextButton(
+                onClick = { onConfirmDelete() }
+            ) {
+                Text(text = stringResource(id = R.string.delete))
+            }
+        },
+        onDismissRequest = onDismissRequest,
+        modifier = modifier
+    )
 }

app/src/main/java/com/suvanl/fixmylinks/viewmodel/rule/RuleDetailsViewModel.kt
@@ -1,25 +1,0 @@
-package com.suvanl.fixmylinks.viewmodel.rule
-
-import androidx.lifecycle.ViewModel
-import com.suvanl.fixmylinks.data.repository.RulesRepository
-import com.suvanl.fixmylinks.domain.mutation.MutationType
-import com.suvanl.fixmylinks.domain.mutation.model.BaseMutationModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import javax.inject.Inject
-
-@HiltViewModel
-class RuleDetailsViewModel @Inject constructor(
-    private val rulesRepository: RulesRepository,
-) : ViewModel() {
-
-    private val _selectedRule = MutableStateFlow<BaseMutationModel?>(null)
-    val selectedRule = _selectedRule.asStateFlow()
-
-    suspend fun updateSelectedRule(baseRuleId: Long, mutationType: MutationType) {
-        rulesRepository.getRuleByBaseId(baseRuleId, mutationType).collect { rule ->
-            _selectedRule.value = rule
-        }
-    }
-}

commit b536f1e5e26d0aab38f3fd47b1da44eac4f92391

🐛 Fix broken edit functionality by passing correct nav arguments to `RuleDetails` and subsequently `AddRule` destinations
This regression was introduced in 947ec660352d5ea8f6786fa584310f1f088b5327.

The 'edit' FAB now shows again and behaves as it did prior to the regressive commit.

app/src/main/java/com/suvanl/fixmylinks/ui/navigation/FmlNavHost.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Text
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -47,6 +48,8 @@ import com.suvanl.fixmylinks.ui.util.getNewRuleFlowViewModel
 import com.suvanl.fixmylinks.viewmodel.RulesViewModel
 import com.suvanl.fixmylinks.viewmodel.newruleflow.AddRuleViewModel
 import com.suvanl.fixmylinks.viewmodel.newruleflow.SelectRuleTypeViewModel
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
 @Composable
@@ -88,26 +91,39 @@ fun FmlNavHost(
                 val viewModel = navBackStackEntry.sharedViewModel<RulesViewModel>(navController)
                 val uiState by viewModel.rulesScreenUiState.collectAsStateWithLifecycle()
 
+                val coroutineScope = rememberCoroutineScope()
+
                 RulesScreen(
                     uiState = uiState,
                     onClickRuleItem = { rule ->
-                        viewModel.setSelectedRule(rule)
-                        navController.navigateSingleTop(
-                            route = FmlScreen.RuleDetails.route,
-                            popUpToStartDestination = false
-                        )
+                        coroutineScope.launch {
+                            viewModel.setSelectedRule(rule)
+
+                            // first non-null emission
+                            val selectedRule = viewModel.selectedRule.filter { it != null }.first()
+
+                            navController.navigateSingleTop(
+                                route = "${FmlScreen.RuleDetails.route}/${selectedRule?.mutationType}/${selectedRule?.baseRuleId}",
+                                popUpToStartDestination = false
+                            )
+                        }
                     }
                 )
             }
 
             composable(
-                route = FmlScreen.RuleDetails.route
+                route = FmlScreen.RuleDetails.routeWithArgs,
+                arguments = FmlScreen.RuleDetails.args
             ) { navBackStackEntry ->
                 val viewModel = navBackStackEntry.sharedViewModel<RulesViewModel>(navController)
                 val selectedRule by viewModel.selectedRule.collectAsStateWithLifecycle()
                 val deleteConfirmationRequired by viewModel.deleteConfirmationRequired.collectAsStateWithLifecycle()
 
                 val coroutineScope = rememberCoroutineScope()
+
+                LaunchedEffect(key1 = Unit) {
+                    viewModel.refreshSelectedRule()
+                }
 
                 ProvideAppBarActions {
                     OverflowMenu {

app/src/main/java/com/suvanl/fixmylinks/viewmodel/RulesViewModel.kt
@@ -42,6 +42,17 @@ class RulesViewModel @Inject constructor(
         _deleteConfirmationRequired.value = isRequired
     }
 
+    suspend fun refreshSelectedRule() {
+        if (_selectedRule.value == null) return
+
+        rulesRepository.getRuleByBaseId(
+            _selectedRule.value!!.baseRuleId,
+            _selectedRule.value!!.mutationType
+        ).collect { rule ->
+            _selectedRule.value = rule
+        }
+    }
+
     suspend fun deleteSingleRule(baseRuleId: Long) {
         setSelectedRule(null)
         rulesRepository.deleteByBaseRuleId(baseRuleId)