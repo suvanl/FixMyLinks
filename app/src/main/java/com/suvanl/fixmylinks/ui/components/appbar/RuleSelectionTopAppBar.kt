61568bd50284a23e0d70b966f793a4ad9fcc9835
suvanl <32813609+suvanl@users.noreply.github.com>
2023-12-16 18:27:02 +0000

    💄 (RuleSelectionTopAppBar): Update container colour

--- app/src/main/java/com/suvanl/fixmylinks/ui/components/appbar/RuleSelectionTopAppBar.kt
+++ app/src/main/java/com/suvanl/fixmylinks/ui/components/appbar/RuleSelectionTopAppBar.kt
@@ -13,11 +13,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.produceState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.FloatingWindow
 import androidx.navigation.NavBackStackEntry
 import com.suvanl.fixmylinks.ui.util.PreviewContainer
@@ -68,7 +70,7 @@ private fun RuleSelectionTopAppBarBody(
                 )
             }
         },
-        colors = TopAppBarDefaults.topAppBarColors(containerColor = MaterialTheme.colorScheme.surfaceVariant),
+        colors = TopAppBarDefaults.topAppBarColors(containerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(3.dp)),
         actions = actions,
         modifier = modifier
     )
