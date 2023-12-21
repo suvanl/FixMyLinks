1a830359e24b68a1dd08ced384e706cf99ece1c1
suvanl <32813609+suvanl@users.noreply.github.com>
2023-12-21 18:51:05 +0000

    🏗️ Improve testability of FmlTopAppBar
    Also added a preview for this component

--- app/src/main/java/com/suvanl/fixmylinks/ui/components/appbar/TopAppBar.kt
+++ app/src/main/java/com/suvanl/fixmylinks/ui/components/appbar/TopAppBar.kt
@@ -1,5 +1,7 @@
 package com.suvanl.fixmylinks.ui.components.appbar
 
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -9,17 +11,20 @@ import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.produceState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.navigation.FloatingWindow
 import androidx.navigation.NavBackStackEntry
 import com.suvanl.fixmylinks.R
 import com.suvanl.fixmylinks.ui.theme.LetterSpacingDefaults
+import com.suvanl.fixmylinks.ui.util.PreviewContainer
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.filterNot
 
@@ -44,12 +49,33 @@ fun FmlTopAppBar(
         }
     )
 
+    TopAppBarBody(
+        title = title,
+        size = size,
+        scrollBehavior = scrollBehavior,
+        onNavigateUp = onNavigateUp,
+        modifier = modifier,
+    ) {
+        AppBarActionRow(navBackStackEntry = currentContentBackStackEntry)
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBarBody(
+    title: String,
+    size: TopAppBarSize,
+    scrollBehavior: TopAppBarScrollBehavior,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    actions: @Composable RowScope.() -> Unit = {},
+) {
     when (size) {
         TopAppBarSize.SMALL -> {
             TopAppBar(
                 title = { TopAppBarTitle(text = title) },
                 navigationIcon = { TopAppBarNavigationIcon(onNavigateUp = onNavigateUp) },
-                actions = { AppBarActionRow(navBackStackEntry = currentContentBackStackEntry) },
+                actions = actions,
                 scrollBehavior = scrollBehavior,
                 modifier = modifier
             )
@@ -59,7 +85,7 @@ fun FmlTopAppBar(
             MediumTopAppBar(
                 title = { TopAppBarTitle(text = title) },
                 navigationIcon = { TopAppBarNavigationIcon(onNavigateUp = onNavigateUp) },
-                actions = { AppBarActionRow(navBackStackEntry = currentContentBackStackEntry) },
+                actions = actions,
                 scrollBehavior = scrollBehavior,
                 modifier = modifier
             )
@@ -69,7 +95,7 @@ fun FmlTopAppBar(
             LargeTopAppBar(
                 title = { TopAppBarTitle(text = title) },
                 navigationIcon = { TopAppBarNavigationIcon(onNavigateUp = onNavigateUp) },
-                actions = { AppBarActionRow(navBackStackEntry = currentContentBackStackEntry) },
+                actions = actions,
                 scrollBehavior = scrollBehavior,
                 modifier = modifier
             )
@@ -114,6 +140,25 @@ private fun TopAppBarNavigationIcon(
         Icon(
             imageVector = Icons.Default.ArrowBack,
             contentDescription = stringResource(id = R.string.navigate_up)
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(widthDp = 320)
+@Preview(
+    name = "Dark",
+    widthDp = 320,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+@Composable
+private fun FmlTopAppBarPreview() {
+    PreviewContainer {
+        TopAppBarBody(
+            title = "Home",
+            size = TopAppBarSize.SMALL,
+            scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(),
+            onNavigateUp = {},
         )
     }
 }
