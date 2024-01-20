8c8e0166560322c115c2875f3c4dad9bb7518089
suvanl <32813609+suvanl@users.noreply.github.com>
2024-01-20 15:27:43 +0000

    ✨ (ui): Add tabs to RulesScreen
    Introduces separation between custom and built-in rules.
    
    As the control flow logic that determines the composable to display when a specific tab is selected is within a HorizontalPager, the tabs can be swiped between (in addition to the default click behaviour).

--- app/src/main/java/com/suvanl/fixmylinks/ui/screens/RulesScreen.kt
+++ app/src/main/java/com/suvanl/fixmylinks/ui/screens/RulesScreen.kt
@@ -1,21 +1,32 @@
 package com.suvanl.fixmylinks.ui.screens
 
 import android.content.res.Configuration
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Link
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -37,6 +48,11 @@ data class RulesScreenUiState(
     val rules: List<BaseMutationModel> = listOf()
 )
 
+data class TabItem(
+    val title: String
+)
+
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun RulesScreen(
     uiState: RulesScreenUiState,
@@ -45,15 +61,84 @@ fun RulesScreen(
     onUpdateSelectedItems: (Set<BaseMutationModel>) -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    val tabItems = listOf(
+        TabItem(title = stringResource(R.string.custom)),
+        TabItem(title = stringResource(R.string.built_in)),
+    )
+
     val hasRules = uiState.rules.isNotEmpty()
 
+    Column(
+        modifier = modifier.semantics { contentDescription = "Rules Screen" }
+    ) {
+        var selectedTabIndex by remember { mutableIntStateOf(0) }
+        val pagerState = rememberPagerState { tabItems.size }
+
+        LaunchedEffect(selectedTabIndex) {
+            pagerState.animateScrollToPage(selectedTabIndex)
+        }
+
+        LaunchedEffect(pagerState.currentPage, pagerState.isScrollInProgress) {
+            if (!pagerState.isScrollInProgress) {
+                selectedTabIndex = pagerState.currentPage
+            }
+        }
+
+        TabRow(selectedTabIndex = selectedTabIndex, modifier = Modifier.padding(vertical = 8.dp)) {
+            tabItems.forEachIndexed { index, item ->
+                Tab(
+                    selected = index == selectedTabIndex,
+                    onClick = { selectedTabIndex = index },
+                    text = {
+                        Text(
+                            text = item.title,
+                            letterSpacing = LetterSpacingDefaults.Tight
+                        )
+                    }
+                )
+            }
+        }
+
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1F)
+        ) { tabIndex ->
+            when (tabIndex) {
+                0 -> {
+                    CustomRulesBody(
+                        uiState = uiState,
+                        hasRules = hasRules,
+                        onClickRuleItem = onClickRuleItem,
+                        selectedItems = selectedItems,
+                        onUpdateSelectedItems = onUpdateSelectedItems
+                    )
+                }
+
+                1 -> {
+                    Text(text = "Built-in rules")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CustomRulesBody(
+    uiState: RulesScreenUiState,
+    hasRules: Boolean,
+    onClickRuleItem: (BaseMutationModel) -> Unit,
+    selectedItems: Set<BaseMutationModel>,
+    onUpdateSelectedItems: (Set<BaseMutationModel>) -> Unit,
+    modifier: Modifier = Modifier
+) {
     Column(
         horizontalAlignment = if (hasRules) Alignment.Start else Alignment.CenterHorizontally,
         verticalArrangement = if (hasRules) Arrangement.Top else Arrangement.Center,
         modifier = modifier
             .fillMaxSize()
             .padding(horizontal = 16.dp)
-            .semantics { contentDescription = "Rules Screen" }
     ) {
         if (!hasRules) {
             EmptyRulesBody()
