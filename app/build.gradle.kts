577c249017a61cf7562db708e91fa8634f9ba4a0
suvanl <32813609+suvanl@users.noreply.github.com>
2024-03-09 16:21:39 +0000

    ⬆️ Upgrade Compose BOM to latest version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,7 +86,7 @@ dependencies {
 
     implementation("androidx.datastore:datastore-preferences:$preferencesDataStoreVersion")
 
-    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
+    implementation(platform("androidx.compose:compose-bom:2024.02.02"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -109,7 +109,7 @@ dependencies {
     testImplementation("org.mockito.kotlin:mockito-kotlin:4.1.0") // v5 drops Java 8 support
     testImplementation("androidx.room:room-testing:$roomVersion")
 
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.01"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.02.02"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 
     androidTestImplementation("androidx.test:runner:1.5.2")
